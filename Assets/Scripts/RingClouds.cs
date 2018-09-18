a4744b724e5380aae69e61e61fa343d2adefdb1a
IT-VRBOX3\Nick <nsaunderswps@gmail.com>
2018-09-18 17:04:54 -0400

    PUNCH

---
+++ Assets/Scripts/RingClouds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingClouds : MonoBehaviour {
+
+	Camera mainCamera;
+	GameObject target;
+	bool look;
+
+	void Start () {
+		mainCamera = Camera.main;
+		target = mainCamera.GetComponent<RayCastBoom> ().mainColl.gameObject;
+		Invoke ("StopLooking", 0.1f);
+		Invoke ("Delete", 4);
+		look = true;
+
+	}
+	
+
+	void Update () {
+		if (look) {
+			transform.LookAt (target.transform);
+		}
+		transform.Translate (Vector3.back * Time.deltaTime * 50);
+	}
+	void StopLooking(){
+		look = false;
+
+	}
+	void Delete(){
+
+		Destroy (gameObject);
+	}
+}
